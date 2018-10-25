531f4be15d806a8e0ad4e932f6eb1938e3bc2622
s107808 <tien.lin@iag.com.au>
2018-10-25 17:20:25 +1100

    update message to claim

--- slack/slack-lambda.js
+++ slack/slack-lambda.js
@@ -2,14 +2,14 @@ const https = require('https');
 
 exports.handler = (event, context, callback) => {
   const payload = JSON.stringify({
-    text: `Claim made by ${event.name}\n Constact details: (${event.email}):\n 
-    ${event.message}`,
+    text: `Claim made by ${event.name}\n Constact details: (${event.email})
+    Claim details: ${event.claim}`,
   });
 
   const options = {
     hostname: 'hooks.slack.com',
     method: 'POST',
-    path: `/services/${process.env.SLACK_API_TOKEN}`,
+    path: `/services/${proccess.env.SLACK_API_TOKEN}`,
   };
 
   const req = https.request(options, res =>
