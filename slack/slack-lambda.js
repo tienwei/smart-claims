089a4b6247ba6bd6153f1b012172430693726bd8
s107808 <tien.lin@iag.com.au>
2018-10-25 15:19:31 +1100

    integrate with slack

--- slack/slack-lambda.js
+++ slack/slack-lambda.js
@@ -1,4 +1,20 @@
+const https = require('https');
+
 exports.handler = (event, context, callback) => {
-  // TODO implement
-  callback(null, 'Hello from Lambda');
+  const payload = JSON.stringify({
+    text: `Message sent by ${event.name} (${event.email}):\n ${event.message}`,
+  });
+
+  const options = {
+    hostname: 'hooks.slack.com',
+    method: 'POST',
+    path: '/services/TDMANCVNV/BDN556SG5/Uus71lTLXIpi9O4uWF3g6XWT',
+  };
+
+  const req = https.request(options, res =>
+    res.on('data', () => callback(null, 'OK')),
+  );
+  req.on('error', error => callback(JSON.stringify(error)));
+  req.write(payload);
+  req.end();
 };
